67ff5ddb8ced305df797a5276d81f36e5de20e4b
Your Name <you@example.com>
2018-05-24 22:11:09 +0800

    expres

--- express/index.js
+++ express/index.js
@@ -6,21 +6,31 @@ function createApplication() {
     let { pathname } = url.parse(req.url, true);
     // 通过next方法进行迭代
     let index = 0;
-    function next() {
-      // 如果数组全部迭代完成还没有找到 说明路径不存在
-      if(index === app.routes.length) return res.end(`Cannot ${m} ${pathname}`);
+    function next(err) {
+      if (index === app.routes.length) return res.end(`Cannot ${m} ${pathname}`);
       let { method, path, handler } = app.routes[index++]; // 每次调用next就应该取下一个layer
-      if (method === 'middle') { // 处理中间件
-        if(path === '/' || path === pathname || pathname.startsWith(path+'/')){
-          handler(req,res,next);
+      if(err){
+        // 如果有错误 我们应该去找错误中间件，错误中间件有一个特点（handler有四个参数）
+        if(handler.length === 4){
+          handler(err,req,res,next); 
         }else{
-          next(); // 如果这个中间件没有匹配到 那么继续走下一个层匹配
+          //如果没有匹配到 要将err继续传递下去
+          next(err);// 继续走下一个layer继续判断
         }
-      } else { // 处理路由
-        if ((method === m || method === 'all') && (path === pathname || path === '*')) {
-          handler(req, res);
-        }else{
-          next();
+      }else{
+        // 如果数组全部迭代完成还没有找到 说明路径不存在
+        if (method === 'middle') { // 处理中间件
+          if (path === '/' || path === pathname || pathname.startsWith(path + '/')) {
+            handler(req, res, next);
+          } else {
+            next(); // 如果这个中间件没有匹配到 那么继续走下一个层匹配
+          }
+        } else { // 处理路由
+          if ((method === m || method === 'all') && (path === pathname || path === '*')) {
+            handler(req, res);
+          } else {
+            next();
+          }
         }
       }
     }
@@ -39,6 +49,14 @@ function createApplication() {
     }
     app.routes.push(layer); // 将中间件放到容器内
   }
+  app.use(function (req,res,next) { // express内置中间件
+    let {pathname,query} = url.parse(req.url,true);
+    let hostname = req.headers['host'].split(':')[0];
+    req.path = pathname;
+    req.query = query;
+    req.hostname = hostname;
+    next();
+  })  
   app.all = function (path, handler) {
     let layer = {
       method: 'all',
@@ -66,3 +84,7 @@ function createApplication() {
   return app;
 }
 module.exports = createApplication;
+// 路径参数 /article/:id/:name
+// express 自路由
+// res封装
+// 模板的渲染
